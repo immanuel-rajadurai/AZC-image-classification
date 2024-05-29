7e74ef20005927284c3bea9a20e0a62199efa1b8
nis-sin <nischalgurungji@gmail.com>
2024-05-30 00:06:37 +0100

    Updated script and added 1000 entries to csv

--- src/preprocessing/convert_animal_names.py
+++ src/preprocessing/convert_animal_names.py
@@ -1,33 +1,55 @@
 import pandas as pd
 import pyinaturalist as pynat
 
-# Read the animal names in Latin
-df = pd.read_csv("data/animal_latin_colloquial.csv")
-print("----------------------Running------------------------------")
 
-# Print the generated response
-print("Answering the question...")
-for i in range(50):
-    latinName = df.loc[i, "Latin Names"]
+def retrieveColloquialName(df: pd.DataFrame) -> None:
+    """
+    Retrieve the colloquial names of the animals in the dataframe
+    """
 
-    # Retrieve the taxa
-    response = pynat.get_taxa(q=latinName, rank = ["species"])
-    taxa = pynat.Taxon.from_json_list(response)
+    i = 0           # use i = 1000
+    while i < 10000:
+        latinName = df.loc[i, "Latin Names"]
 
-    # Check if there is a response
-    if len(taxa) == 0:
-        print("No response for", latinName)
-        break
+        # Retrieve the taxa
+        response = pynat.get_taxa(q=latinName, rank = ["species"])
+        taxa = pynat.Taxon.from_json_list(response)
 
-    # Check if there is a colloquial name
-    colloquialName = taxa[0].preferred_common_name
-    if colloquialName in (None, "", " "):
-        print("No colloquial name for", latinName)
-        break
+        # Check if there is a response
+        if len(taxa) == 0:
+            print("No response for", latinName)
+            df.loc[i, "Colloquial Names"] = "Species not found"
+            continue
 
-    # Update the dataframe with the colloquial name
-    df.loc[i, "Colloquial Names"] = colloquialName
+        # Check if there is a colloquial name
+        colloquialName = taxa[0].preferred_common_name
+        if colloquialName in (None, "", " "):
+            print("No colloquial name for", latinName)
+            df.loc[i, "Colloquial Names"] = "No colloquial name"
+        else:
+            # Update the dataframe with the colloquial name
+            df.loc[i, "Colloquial Names"] = colloquialName
 
-print("Done!")
-# Save the dataframe
-df.to_csv("data/animal_latin_colloquial.csv",index=False)
+        i+=1
+        if i % 100 == 0:
+            print(i, "done")
+            ask = input("Do you want to continue? (y/n)")
+            ask = ask.lower()
+            if ask == "n":
+                break
+
+if __name__ == "__main__":
+
+    # Read the animal names in Latin
+    df = pd.read_csv("data/animal_latin_colloquial.csv")
+    print("----------------------Running------------------------------")
+
+    # Retrieve the colloquial names of the animals
+    retrieveColloquialName(df)
+    # Print the generated response
+    print("Answering the question...")
+
+
+    print("Done!")
+    # Save the dataframe
+    df.to_csv("data/animal_latin_colloquial.csv",index=False)
