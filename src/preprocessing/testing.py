a3e9f3b98dd3f46c8d4acc32b780979b136e6a71
nis-sin <nischalgurungji@gmail.com>
2024-05-29 09:46:27 +0100

    Deleted testing.py file

--- src/preprocessing/testing.py
+++
@@ -1,8 +0,0 @@
-from pyinaturalist import *
-
-response = get_taxa(q='lumbricus terrestris', rank = ["species"])
-print()
-print("Answer")
-taxa = Taxon.from_json_list(response)
-
-print(taxa[0].preferred_common_name)
