8792e89badf122feeae6a04e1a37d5e8ace20829
nis-sin <nischalgurungji@gmail.com>
2024-05-29 09:45:34 +0100

    Updated script to use pyinaturalist package to convert latin to colloquial names

--- src/preprocessing/convert_animal_names.py
+++ src/preprocessing/convert_animal_names.py
@@ -1,57 +1,33 @@
 import pandas as pd
-from openai import OpenAI
-import os
-from dotenv import load_dotenv
+import pyinaturalist as pynat
 
 # Read the animal names in Latin
-df = pd.read_csv("data/animal_names_latin.txt", header=None, names = ["Latin Names"])
-# Remove the underscores in the names and convert to a dataframe
-df = (df["Latin Names"].apply(lambda x: " ".join(x.split("_")))).to_frame()
-df["Colloquial Names"] = ""
+df = pd.read_csv("data/animal_latin_colloquial.csv")
+print("----------------------Running------------------------------")
 
-df.to_csv("data/animal_latin_colloquial.csv",index=False)
-
-# Initialize the OpenAI client with your API key
-load_dotenv()
-API_KEY = os.getenv("OPENAI_API_KEY")
-
-client = OpenAI(api_key=API_KEY)  # Replace with your actual API key
- 
-"""
-latinName = df.loc[3, "Latin Names"]
-print("Answering the question...")
-conversation = [
-    {"role": "system", "content": "You are an expert taxonomist, skilled as giving the right colloquial equivalent latin names. When given a name, your responses should be straight to the point, that is you should strictly give the colloquial name only. Eg: When I ask for the colloquial name of: atypoides riversi, your response should be: crab and not River crab. Let's get started!"},
-    {"role": "user", "content": f"Give me just the high level colloquial species name, no adjectives, of: {latinName}."}
-]
-
-# Generate a chat completion
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=conversation,
-    temperature=0
-)
-print("Done!")
-print(completion.choices[0].message.content)
-df.loc[3, "Colloquial Names"] = completion.choices[0].message.content
-
-"""
 # Print the generated response
 print("Answering the question...")
-for i in range(10):
+for i in range(50):
     latinName = df.loc[i, "Latin Names"]
-    conversation = [
-        {"role": "system", "content": "You are an expert taxonomist, skilled as giving the right colloquial equivalent latin names. When given a name, your responses should be straight to the point, that is you should strictly give the colloquial name only. Eg: When I ask for the colloquial name of: atypoides riversi, your response should be: River crab. And for latin names you are uncertain about, just give the high level species name with no adjectives. Let's get started!"},
-        {"role": "user", "content": f"Give me just the high level colloquial name of: {latinName}. When given a name, your responses should be straight to the point, that is you should strictly give the colloquial name only. Eg: When I ask for the colloquial name of: atypoides riversi, your response should be: River crab. And for latin names you are uncertain about, just give the high level species name with no adjectives."}
-    ]
 
-    # Generate a chat completion
-    completion = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=conversation,
-        temperature=0
-    )
-    
-    df.loc[i, "Colloquial Names"] = completion.choices[0].message.content
+    # Retrieve the taxa
+    response = pynat.get_taxa(q=latinName, rank = ["species"])
+    taxa = pynat.Taxon.from_json_list(response)
+
+    # Check if there is a response
+    if len(taxa) == 0:
+        print("No response for", latinName)
+        break
+
+    # Check if there is a colloquial name
+    colloquialName = taxa[0].preferred_common_name
+    if colloquialName in (None, "", " "):
+        print("No colloquial name for", latinName)
+        break
+
+    # Update the dataframe with the colloquial name
+    df.loc[i, "Colloquial Names"] = colloquialName
+
 print("Done!")
+# Save the dataframe
 df.to_csv("data/animal_latin_colloquial.csv",index=False)
